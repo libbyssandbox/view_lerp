b126640eebd1832b8ae02486ec8435f5c20186b1
Zach <147861361+1054009@users.noreply.github.com>
2024-05-24 02:15:18 -0400

    Use math.AngleDifference for calculations

--- view_lerp/lua/autorun/view_lerp.lua
+++ view_lerp/lua/autorun/view_lerp.lua
@@ -2,11 +2,9 @@ AddCSLuaFile()
 
 require("libbys_sdk")
 
-local remap_angle = libbys_sdk.maths.remap_angle
+local LERP_PERCENTAGE = 0.75
 
 libbys_sdk.util.unique_hook("StartCommand", function(ply, cmd)
-	if not IsFirstTimePredicted() then return end
-
 	if not isangle(ply.m_angDesiredEye) or (cmd:GetMouseX() ~= 0 or cmd:GetMouseY() ~= 0) then
 		ply.m_angDesiredEye = cmd:GetViewAngles()
 	end
@@ -16,21 +14,21 @@ libbys_sdk.util.unique_hook("StartCommand", function(ply, cmd)
 		return
 	end
 
-	local old_pitch = remap_angle(ply.m_angLastEye.pitch)
-	local old_yaw = remap_angle(ply.m_angLastEye.yaw)
-	local old_roll = remap_angle(ply.m_angLastEye.roll)
+	local old_pitch = ply.m_angLastEye.pitch
+	local old_yaw = ply.m_angLastEye.yaw
+	local old_roll = ply.m_angLastEye.roll
 
-	local desired_pitch = remap_angle(ply.m_angDesiredEye.pitch)
-	local desired_yaw = remap_angle(ply.m_angDesiredEye.yaw)
-	local desired_roll = remap_angle(ply.m_angDesiredEye.roll)
+	local desired_pitch = ply.m_angDesiredEye.pitch
+	local desired_yaw = ply.m_angDesiredEye.yaw
+	local desired_roll = ply.m_angDesiredEye.roll
 
-	local new_pitch = Lerp(0.75, old_pitch, desired_pitch)
-	local new_yaw = Lerp(0.75, old_yaw, desired_yaw)
-	local new_roll = Lerp(0.75, old_roll, desired_roll)
+	local pitch_difference = math.AngleDifference(old_pitch, desired_pitch) * LERP_PERCENTAGE
+	local yaw_difference = math.AngleDifference(old_yaw, desired_yaw) * LERP_PERCENTAGE
+	local roll_difference = math.AngleDifference(old_roll, desired_roll) * LERP_PERCENTAGE
 
-	new_pitch = math.NormalizeAngle(new_pitch)
-	new_yaw = math.NormalizeAngle(new_yaw)
-	new_roll = math.NormalizeAngle(new_roll)
+	local new_pitch = math.NormalizeAngle(old_pitch - pitch_difference)
+	local new_yaw = math.NormalizeAngle(old_yaw - yaw_difference)
+	local new_roll = math.NormalizeAngle(old_roll - roll_difference)
 
 	local new_eye = Angle(new_pitch, new_yaw, new_roll)
 
