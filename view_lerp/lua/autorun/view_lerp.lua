6e7f5ae977ac9af93e8274cbc57334d416b5fc5b
Zach <147861361+1054009@users.noreply.github.com>
2024-05-24 02:36:19 -0400

    Create lerp_angle function

--- view_lerp/lua/autorun/view_lerp.lua
+++ view_lerp/lua/autorun/view_lerp.lua
@@ -4,6 +4,18 @@ require("libbys_sdk")
 
 local LERP_PERCENTAGE = 0.75
 
+local function lerp_angle(old, new)
+	local pitch_difference = math.AngleDifference(old.pitch, new.pitch) * LERP_PERCENTAGE
+	local yaw_difference = math.AngleDifference(old.yaw, new.yaw) * LERP_PERCENTAGE
+	local roll_difference = math.AngleDifference(old.roll, new.roll) * LERP_PERCENTAGE
+
+	local new_pitch = math.NormalizeAngle(old.pitch - pitch_difference)
+	local new_yaw = math.NormalizeAngle(old.yaw - yaw_difference)
+	local new_roll = math.NormalizeAngle(old.roll - roll_difference)
+
+	return Angle(new_pitch, new_yaw, new_roll)
+end
+
 libbys_sdk.util.unique_hook("StartCommand", function(ply, cmd)
 	if not isangle(ply.m_angDesiredEye) or (cmd:GetMouseX() ~= 0 or cmd:GetMouseY() ~= 0) then
 		ply.m_angDesiredEye = cmd:GetViewAngles()
@@ -14,23 +26,7 @@ libbys_sdk.util.unique_hook("StartCommand", function(ply, cmd)
 		return
 	end
 
-	local old_pitch = ply.m_angLastEye.pitch
-	local old_yaw = ply.m_angLastEye.yaw
-	local old_roll = ply.m_angLastEye.roll
-
-	local desired_pitch = ply.m_angDesiredEye.pitch
-	local desired_yaw = ply.m_angDesiredEye.yaw
-	local desired_roll = ply.m_angDesiredEye.roll
-
-	local pitch_difference = math.AngleDifference(old_pitch, desired_pitch) * LERP_PERCENTAGE
-	local yaw_difference = math.AngleDifference(old_yaw, desired_yaw) * LERP_PERCENTAGE
-	local roll_difference = math.AngleDifference(old_roll, desired_roll) * LERP_PERCENTAGE
-
-	local new_pitch = math.NormalizeAngle(old_pitch - pitch_difference)
-	local new_yaw = math.NormalizeAngle(old_yaw - yaw_difference)
-	local new_roll = math.NormalizeAngle(old_roll - roll_difference)
-
-	local new_eye = Angle(new_pitch, new_yaw, new_roll)
+	local new_eye = lerp_angle(ply.m_angLastEye, ply.m_angDesiredEye)
 
 	cmd:SetViewAngles(new_eye)
 
