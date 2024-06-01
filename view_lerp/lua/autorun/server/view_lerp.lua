f7991d1167822f83346bffdeae483d3368795c85
Zach <147861361+1054009@users.noreply.github.com>
2024-06-01 05:02:22 -0400

    Rework to use new libs and add limiter

--- view_lerp/lua/autorun/server/view_lerp.lua
+++ view_lerp/lua/autorun/server/view_lerp.lua
@@ -1,20 +1,23 @@
-AddCSLuaFile()
-
-require("libbys_sdk")
+require("libbys")
 libbys_sdk.util.require_binary("worldclicker")
 
-local HAS_WORLDCLICKER = isfunction(libbys_sdk.metatables.get("CUserCmd").GetWorldClicker)
+local LERP_PERCENTAGE = 0.75 -- Percentage of view angles to allow per tick
+local MAX_PER_TICK = 20 -- Maximum amount of angle units allowed per tick
 
-local LERP_PERCENTAGE = 0.75
+local Angle = Angle
+local math_AngleDifference = math.AngleDifference
+local math_Clamp = math.Clamp
+local math_NormalizeAngle = math.NormalizeAngle
 
+-- Modified version of libbys.math.lerp_angle
 local function lerp_angle(old, new)
-	local pitch_difference = math.AngleDifference(old.pitch, new.pitch) * LERP_PERCENTAGE
-	local yaw_difference = math.AngleDifference(old.yaw, new.yaw) * LERP_PERCENTAGE
-	local roll_difference = math.AngleDifference(old.roll, new.roll) * LERP_PERCENTAGE
+	local pitch_difference = math_Clamp(math_AngleDifference(old.pitch, new.pitch) * LERP_PERCENTAGE, -MAX_PER_TICK, MAX_PER_TICK)
+	local yaw_difference = math_Clamp(math_AngleDifference(old.yaw, new.yaw) * LERP_PERCENTAGE, -MAX_PER_TICK, MAX_PER_TICK)
+	local roll_difference = math_Clamp(math_AngleDifference(old.roll, new.roll) * LERP_PERCENTAGE, -MAX_PER_TICK, MAX_PER_TICK)
 
-	local new_pitch = math.NormalizeAngle(old.pitch - pitch_difference)
-	local new_yaw = math.NormalizeAngle(old.yaw - yaw_difference)
-	local new_roll = math.NormalizeAngle(old.roll - roll_difference)
+	local new_pitch = math_NormalizeAngle(old.pitch - pitch_difference)
+	local new_yaw = math_NormalizeAngle(old.yaw - yaw_difference)
+	local new_roll = math_NormalizeAngle(old.roll - roll_difference)
 
 	return Angle(new_pitch, new_yaw, new_roll)
 end
@@ -31,16 +34,14 @@ libbys_sdk.util.unique_hook("StartCommand", function(ply, cmd)
 	ply.m_angLastEye = new_eye
 
 	-- Worldclicker angles
-	if HAS_WORLDCLICKER then
-		if cmd:GetWorldClicker() then
-			ply.m_vecDesiredWorldClicker = cmd:GetWorldClickerAngle()
-			ply.m_vecLastWorldClicker = ply.m_vecLastWorldClicker or ply.m_vecDesiredWorldClicker
+	if cmd:GetWorldClicker() then
+		ply.m_vecDesiredWorldClicker = cmd:GetWorldClickerAngle()
+		ply.m_vecLastWorldClicker = ply.m_vecLastWorldClicker or ply.m_vecDesiredWorldClicker
 
-			local new_worldclicker = lerp_angle(ply.m_vecDesiredWorldClicker:Angle(), ply.m_vecLastWorldClicker:Angle()):Forward()
-				cmd:SetWorldClickerAngle(new_worldclicker)
-			ply.m_vecLastWorldClicker = new_worldclicker
-		else
-			cmd:SetWorldClickerAngle(new_eye:Forward())
-		end
+		local new_worldclicker = lerp_angle(ply.m_vecDesiredWorldClicker:Angle(), ply.m_vecLastWorldClicker:Angle()):Forward()
+			cmd:SetWorldClickerAngle(new_worldclicker)
+		ply.m_vecLastWorldClicker = new_worldclicker
+	else
+		cmd:SetWorldClickerAngle(new_eye:Forward())
 	end
 end)
